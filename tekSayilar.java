329340cf57ea3ede7298f4a48db4ba7d6e43dfe9
MUHAMMED ELNECCAR <120338571+MUHAMMEDELNECCAR@users.noreply.github.com>
2022-12-13 23:59:44 +0300

    Create tekSayilar.java

---
+++ tekSayilar.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author asus
+ */
+public class JavaApplication1 {
+    // 1 ile 100 arasindaki tek sayiları bulma
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       for(int i=0;i<=100;i++){
+       if(i%2!=0){
+           System.out.println("i");
+       }
+       }
+    }
+    
+}
